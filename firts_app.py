cd2816cc4a26d16f6f0bb295d0c7427ea7f75cd6
Schweizer Andreas <schweizer@wara.de>
2024-11-06 14:44:16 +0100

    added first label app content

---
+++ firts_app.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+class MainApp(App):
+    def build(self):
+        return Builder.load_file('first_app.kv')
+    
+if __name__ == '__main__':
+    MainApp().run()
